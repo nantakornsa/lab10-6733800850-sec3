6c859d897532bc818c2d3188363b1a5dfe67e43b
nantakornsa <nantakorn.sa@kkumail.com>
2026-09-25 23:10:53 +0700

    feat: implement getAl

--- src/main/java/com/example/lab10/controller/ProductController.java
+++ src/main/java/com/example/lab10/controller/ProductController.java
@@ -1,8 +1,16 @@
 package com.example.lab10.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.lab10.model.Product;
 import com.example.lab10.service.ProductService;
-import org.springframework.web.bind.annotation.*;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -61,7 +69,7 @@ public class ProductController {
     @GetMapping
     public Flux<Product> getAll() {
         // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        return service.getAll();
     }
 
     /**
